d411035ddd595dd24f5e1c1693d44220a535f7dd
aland <aland@vinova.com.sg>
2024-02-22 13:51:49 +0700

    Update error status code

--- Network.xctemplate/Services/Network/NetworkManager.swift
+++ Network.xctemplate/Services/Network/NetworkManager.swift
@@ -73,7 +73,7 @@ class NetworkManager {
     }
 
     //MARK: - Only work with alamofire
-// func requestJSON(_ endpoint: Endpoint ,isLog: Bool = false, _ completion: @escaping (Result<JSON, Error>) -> Void){
+// func requestJSON(_ endpoint: Endpoint, isLog: Bool = false, _ completion: @escaping (Result<JSON, Error>) -> Void){
 //         if Reachability.isConnectedToNetwork() == false {
 //             completion(.failure(API_ERROR.noInternet))
 //             return
@@ -165,18 +165,10 @@ class NetworkManager {
 //    var header: HTTPHeaders? = nil
 //}
 
-enum RestApiMethod: String {
-    case get = "GET"
-    case post = "POST"
-    case put = "PUT"
-    case delete = "DELETE"
-    case patch = "PATCH"
-}
-
 struct API_ERROR {
-    static let invalidURL = NSError(domain: "", code: 500, userInfo: [ NSLocalizedDescriptionKey: "URL invalid"])
+    static let invalidURL = NSError(domain: "", code: 404, userInfo: [ NSLocalizedDescriptionKey: "URL invalid"])
     static let invailidResponse = NSError(domain: "", code: 500, userInfo: [ NSLocalizedDescriptionKey: "Respose invalid"])
-    static let invailidData = NSError(domain: "", code: 500, userInfo: [ NSLocalizedDescriptionKey: "Data response invalid"])
+    static let invailidData = NSError(domain: "", code: 422, userInfo: [ NSLocalizedDescriptionKey: "Data response invalid"])
     static let noInternet = NSError(domain: "", code: 500, userInfo: [ NSLocalizedDescriptionKey: "Unable to connect to the Internet, please check your connection again!"])
     static let unknown = NSError(domain: "", code: 500, userInfo: [ NSLocalizedDescriptionKey: "Something error"])
 }
